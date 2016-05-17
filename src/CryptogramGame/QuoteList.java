39a577b743032800b9d532084a34de3e05d1ed73
Lucas Pham <90310192@hs-62348-macair.ad.edenpr.org>
2016-05-17 13:03:33 -0500

    QuoteList class

--- src/CryptogramGame/QuoteList.java
+++ src/CryptogramGame/QuoteList.java
@@ -1,5 +1,33 @@
 package CryptogramGame;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class QuoteList {
 
+	private ArrayList<String> quotes;
+	
+	public QuoteList() throws FileNotFoundException {
+		
+		Scanner input = new Scanner(new File("quotes.txt"));
+		this.quotes = new ArrayList<String>();
+		
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			this.quotes.add(line);
+		}
+	}
+	
+	public String getRandomQuote() {
+		
+		int rand = (int) (Math.random() * this.quotes.size());
+		return this.quotes.get(rand);
+	}
+	
+	public ArrayList<String> getQuotes() {
+		
+		return this.quotes;
+	}
 }
