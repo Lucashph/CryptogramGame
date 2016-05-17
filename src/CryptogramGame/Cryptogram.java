6fb4442f59884cce9651b0b763fafbc8ff5710f2
Lucas Pham <90310192@hs-62348-macair.ad.edenpr.org>
2016-05-17 12:28:59 -0500

    Commit

---
+++ src/CryptogramGame/Cryptogram.java
@@ -0,0 +1,36 @@
+package CryptogramGame;
+
+public class Cryptogram {
+
+	private String quotePerson;
+	private String quote;
+	
+	public Cryptogram() {
+		
+		quotePerson = "\"Something interesting\" - Person Person";
+		quote = "\"Something interesting\"";
+	}
+	
+	public Cryptogram(String quotePerson) {
+		
+		this.quotePerson = quotePerson;
+		for (int i = 1; i < quotePerson.length(); i++) {
+			if (quotePerson.substring(i- 1, i).equals("\"")) {
+				for (int j = i; !quotePerson.substring(j, j + 1).equals("\""); j++) {
+					this.quote += quotePerson.substring(j, j + 1);
+				}
+				return;
+			}
+		}
+	}
+	
+	public String getQuotePerson() {
+		
+		return quotePerson;
+	}
+	
+	public String getQuote() {
+		
+		return quote;
+	}
+}
---
+++ src/CryptogramGame/Game.java
@@ -0,0 +1,100 @@
+package CryptogramGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = WIDTH / 12 * 9;
+	public static final int SCALE = 2;
+	public static final String TITLE = "Cryptogram Race";
+	
+	private boolean running = false;
+	private Thread t;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private synchronized void start() {
+		
+		if (running) return;
+		running = true;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	private synchronized void stop() {
+		
+		if (!running) return;
+		running = false;
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.exit(1);
+	}
+	
+	public void run() {
+		
+		long lastTime = System.nanoTime();
+		final double noOfTicks = 60.0;
+		double ns = 1000000000 / noOfTicks;
+		double delta = 0.0;
+		int updates = 0;
+		int frames = 0;
+		long timer = System.currentTimeMillis();
+		
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+		
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		
+		JFrame f = new JFrame(game.TITLE);
+		f.add(game);
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		
+		game.start();
+	}
+}
